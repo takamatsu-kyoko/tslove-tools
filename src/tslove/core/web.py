fe2e5fbe4dc081932787f61e58620ab822270745
Takamatsu.kyoko <38364117+takamatsu-kyoko@users.noreply.github.com>
2021-07-23 15:18:06 +0900

    Webサイトへのリクエストメソッドを整理

--- src/tslove/core/web.py
+++ src/tslove/core/web.py
@@ -10,8 +10,9 @@ import re
 from typing import Optional, Callable
 
 import requests
-from bs4 import BeautifulSoup  # type: ignore
 from PIL import Image  # type: ignore
+
+from tslove.core.exception import RetryCountExceededError
 
 warnings.filterwarnings('ignore', 'Unverified HTTPS request is being made.')
 
@@ -27,13 +28,110 @@ class TsLoveWeb:
         '''
         :param url: T'sLove の起点URL
         '''
-        self.url = url
-        self._cookies: dict = {}
-        self.last_retries = 0
-        self._request_header = {
-            'User-Agent': 'tslove-tools written by T.Kyoko (tslove member_id=45642)',
-        }
-        self._sns_session_id: Optional[str] = None
+        self.__url = url
+        self.__session = requests.Session()
+        self.__session.headers.update({
+            'User-Agent': 'tslove-tools written by T.Kyoko (tslove member_id=45642)'})
+        self.__php_session_id: Optional[str] = None
+        self.__sns_session_id: Optional[str] = None
+
+        self.__retry_count = 0
+
+    def __del__(self):
+        self.__session.close()
+
+    @property
+    def php_session_id(self) -> Optional[str]:
+        '''PHPSESSID'''
+        return self.__php_session_id
+
+    @property
+    def sns_session_id(self) -> Optional[str]:
+        '''sns_session_id'''
+        return self.__sns_session_id
+
+    @property
+    def retry_count(self) -> int:
+        '''retry_count'''
+        return self.__retry_count
+
+    def __request(self, request: Callable, message: Callable = None) -> requests.Response:
+        '''T'sLoveへリクエストを発行します
+
+        RETRY_COUNT, RETRY_INTERVAL, RETRY_ADDITIONAL の値に従って
+        再試行を行いながら T'sLove へのリクエストを発行します
+
+        実際のリクエストは引数 request で指定します
+        messageが与えられた場合、リトライの発生時にmessageの戻り値をprintします
+
+        :param request: リクエストを発行する関数
+        :param message: リトライメッセージを生成する関数
+        :returns: request.Respose オブジェクト
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
+
+        '''
+        while self.__retry_count < RETRY_COUNT:
+            if self.__retry_count != 0:
+                interval = RETRY_INTERVAL + (self.__retry_count - 1) * RETRY_ADDITIONAL
+                if message:
+                    print(message(interval))
+                time.sleep(interval)
+
+            response = request()
+            if response.ok:
+                return response
+
+            self.__retry_count += 1
+
+        raise RetryCountExceededError()
+
+    def __get(self, path: str, params: dict = None) -> requests.Response:
+        '''T'sLoveからデータをGETします
+
+        :param path: url path
+        :param params: クエリパラメータ
+        :returns: requests.Response オブジェクト
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
+        '''
+        def request() -> requests.Response:
+            url = self.__url + path if path else self.__url
+            return self.__session.get(url, params=params, verify=False, allow_redirects=False, timeout=15)
+
+        def message(interval: int) -> str:
+            msg = 'Retry GET'
+            msg += ' path:{}'.format(path) if path else ''
+            if params:
+                msg += ' action:{}'.format(params['a']) if 'a' in params else ''
+                msg += ' file:{}'.format(params['filename']) if 'filename' in params else ''
+            msg += ' after {} sec.'.format(interval)
+            return msg
+
+        return self.__request(request, message)
+
+    def __post(self, path: str, payload: dict = None) -> requests.Response:
+        '''T'sLoveへデータをPOSTします
+
+        :param path: url path
+        :param payload: POSTデータ
+        :returns: requests.Response オブジェクト
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
+        '''
+        def request() -> requests.Response:
+            url = self.__url + path if path else self.__url
+            return self.__session.post(url, data=payload, verify=False, allow_redirects=False, timeout=15)
+
+        def message(interval: int) -> str:
+            msg = 'Retry POST'
+            msg += ' path:{}'.format(path) if path else ''
+            if payload:
+                msg += ' action:{}'.format(payload['a']) if 'a' in payload else ''
+            msg += ' after {} sec.'.format(interval)
+            return msg
+
+        return self.__request(request, message)
 
     def login(self, username: str, password: str, php_session_id: str = None) -> bool:
         '''T'sLoveへのログインをおこないます
@@ -46,26 +144,32 @@ class TsLoveWeb:
         :param password: パスワード
         :param php_session_id: PHPSESSID
         :return: sns_session_idを取得できた場合 True
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
         if php_session_id is None:
-            self._cookies['PHPSESSID'] = self._get_php_session_id(username, password)
+            self.__php_session_id = self.__get_php_session_id(username, password)
         else:
-            self._cookies['PHPSESSID'] = php_session_id
+            self.__php_session_id = php_session_id
+            self.__session.cookies.set('PHPSESSID', php_session_id)
 
-        self._sns_session_id = self._get_sns_session_id()
+        if self.__php_session_id is None:
+            return False
 
-        if self._sns_session_id is not None:
+        self.__sns_session_id = self.__get_sns_session_id()
+        if self.__sns_session_id is not None:
             return True
 
         return False
 
-    def _get_php_session_id(self, username: str, password: str) -> Optional[str]:
+    def __get_php_session_id(self, username: str, password: str) -> Optional[str]:
         '''PHPSESSIDを取得します
 
         :param username: ユーザ名
         :param password: パスワード
         :return: PHPSESSID もしくは None
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
         payload = {'username': username,
                    'password': password,
@@ -75,107 +179,80 @@ class TsLoveWeb:
                    'is_save': '1',
                    }
 
-        for count in range(RETRY_COUNT):
-            if count != 0:
-                interval = RETRY_INTERVAL + (count - 1) * RETRY_ADDITIONAL
-                print('Retry do_o_login after {} sec.'.format(interval))
-                time.sleep(interval)
+        self.__retry_count = 0
 
-            response = requests.post(self.url, headers=self._request_header, data=payload, verify=False, allow_redirects=False, timeout=15)
-            response.raise_for_status()
+        while True:
+            response = self.__post('', payload)
 
             if 'PHPSESSID' in response.cookies:
-                self.last_retries += count
                 return response.cookies['PHPSESSID']
-            if response.status_code == 302:
-                self.last_retries += count
+            if response.status_code == 302:  # 認証失敗
                 return None
 
-            # DB Error: connect failed ページが200を返してくる
+            self.__retry_count += 1
 
-        raise RuntimeError('retry counter expiered')
-
-    def _get_sns_session_id(self) -> Optional[str]:
+    def __get_sns_session_id(self) -> Optional[str]:
         '''T'sLove session_id を取得します
 
         マイページ確認ページを取得してログアウトのリンクから session_id を取得します
 
         :return: session_id もしくは None
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
-        pattern = re.compile(r'\./\?m=pc&a=do_inc_page_header_logout&sessid=(?P<session_id>.+)')
+        session_id_pattern = re.compile(r'a=do_inc_page_header_logout&amp;sessid=(?P<session_id>.+)"')
 
-        soup = BeautifulSoup(self.get_myprofile_page(), 'html.parser')
-        logout_label = soup.find('li', id='globalNav_9')
-        if logout_label is None:
-            return None
+        params = {'m': 'pc',
+                  'a': 'page_h_prof',
+                  }
+        page = self.get_page(params)
 
-        result = pattern.match(logout_label.a['href'])
+        result = session_id_pattern.search(page)
         if result:
             return result.group('session_id')
 
         return None
-
-    def get_contents(self, path: str = '', params: dict = None, cond: Callable = None) -> requests.Response:
-        '''コンテンツを取得します
-
-        :param path: url path
-        :param params: クエリパラメータ
-        :param cond: コンテンツの取得確認関数
-        :return: requests Respose オブジェクト
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
-        '''
-        if params is None:
-            params = {}
-        for count in range(RETRY_COUNT):
-            if count != 0:
-                interval = RETRY_INTERVAL + (count - 1) * RETRY_ADDITIONAL
-                msg = 'Retry'
-                msg += ' path:{}'.format(path) if path else ''
-                msg += ' action:{}'.format(params['a']) if 'a' in params else ''
-                msg += ' file:{}'.format(params['filename']) if 'filename' in params else ''
-                msg += ' after {} sec.'.format(interval)
-                print(msg)
-                time.sleep(interval)
-
-            response = requests.get(self.url + path, headers=self._request_header, params=params, cookies=self._cookies, verify=False, timeout=15)
-            response.raise_for_status()
-
-            if cond is None or cond(response):
-                self.last_retries += count
-                return response
-
-        raise RuntimeError('retry counter expiered')
 
     def get_page(self, params: dict) -> str:
         '''ページを取得します
 
         :param params: クエリパラメータ
         :return: ページの内容
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
-        def cond(response):
-            title_pattern = re.compile(r'<title>(?P<title>.+)</title>')
-            result = title_pattern.search(response.text)
-            return result and not result.group('title') == 'ページが表示できませんでした'
+        self.__retry_count = 0
 
-        response = self.get_contents(params=params, cond=cond)
-        return response.text
+        title_pattern = re.compile(r'<title>(?P<title>.+)</title>')
+        while True:
+            response = self.__get('', params)
+
+            if not response.headers['Content-Type'].startswith('text/html'):
+                self.__retry_count += 1
+                continue
+
+            result = title_pattern.search(response.text)
+            if result and not result.group('title') == 'ページが表示できませんでした':
+                return response.text
+
+            self.__retry_count += 1
 
     def get_stylesheet(self) -> str:
         '''スタイルシートを取得します
 
         :return: スタイルシートの内容
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
-        def cond(response):
-            response.encoding = response.apparent_encoding
+        self.__retry_count = 0
 
-            pattern = re.compile(r'body, div, p, pre, blockquote, th, td,')
-            return pattern.search(response.text)
+        while True:
+            response = self.__get('xhtml_style.php', None)
 
-        response = self.get_contents(path='xhtml_style.php', cond=cond)
-        return response.text
+            if response.headers['Content-Type'].startswith('text/css'):
+                return response.text
+
+            self.__retry_count += 1
 
     def get_image(self, path: str, params: dict = None) -> Image:
         '''画像を取得します
@@ -186,49 +263,58 @@ class TsLoveWeb:
         :param path: url path
         :param params: クエリパラメータ
         :return: PIL Image オブジェクト
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
-        def cond(response):
-            return response.headers['Content-Type'].startswith('image/') or\
-                (response.headers['Content-Type'] == 'text/html' and response.headers['Content-Length'] == '0')
+        self.__retry_count = 0
 
-        response = self.get_contents(path, params, cond)
-        if response.headers['Content-Type'].startswith('image/'):
-            return Image.open(io.BytesIO(response.content))
+        while True:
+            response = self.__get(path, params)
 
-        return Image.new("1", (1, 1), 1)
+            if response.headers['Content-Type'].startswith('image/'):
+                return Image.open(io.BytesIO(response.content))
+
+            if response.headers['Content-Type'].startswith('text/html') and response.headers['Content-Length'] == '0':
+                return Image.new("1", (1, 1), 1)
+
+            self.__retry_count += 1
 
     def get_javascript(self, path: str) -> str:
         '''JavaScriptを取得します
 
         :param path: url path
         :return: JavaScriptファイルの内容
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RequestException: requetsの処理に失敗した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
+        self.__retry_count = 0
 
-        def cond(response):
-            return response.headers['Content-Type'] == 'text/javascript'
+        while True:
+            response = self.__get(path, None)
 
-        response = self.get_contents(path, cond=cond)
-        return response.text
+            if response.headers['Content-Type'].startswith('text/javascript'):
+                return response.text
 
+            self.__retry_count += 1
+
+    # TODO: 別モジュールへ移す
     def get_myprofile_page(self) -> str:
         '''マイページ確認ページを取得します
 
         :return: ページの内容
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
+        :raises RetryCountExceededError: リトライ回数が基準を超過した場合
         '''
         params = {'m': 'pc',
                   'a': 'page_h_prof',
                   }
         return self.get_page(params)
 
+    # TODO: 別モジュールへ移す
     def get_diary_page(self, diary_id: str) -> str:
         '''日記を取得します
 
         :param diary_id: diary_id
         :return: 日記の内容
-        :raises RuntimeErorr: リトライ回数が基準を超過した場合
         :raises RuntimeErorr: 指定したdiary_idの日記が見つからなかった場合
         '''
         params = {'m': 'pc',
