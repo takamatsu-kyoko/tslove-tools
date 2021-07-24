37431f09cecb264718ef3a00b792cfd991b93d05
Takamatsu.kyoko <38364117+takamatsu-kyoko@users.noreply.github.com>
2021-07-24 12:38:00 +0900

    例外クラスの整理

--- src/tslove/core/exception.py
+++ src/tslove/core/exception.py
@@ -4,5 +4,21 @@ tslove-tools全般で使用する例外
 '''
 
 
-class RetryCountExceededError(RuntimeError):
+class TsLoveToolsException(Exception):
+    '''tslove-tools例外の基底クラス'''
+
+
+class WebAccessError(TsLoveToolsException):
+    '''コンテンツの取得に失敗した際に送出されます'''
+
+
+class RequestError(WebAccessError):
+    '''requestsパッケージのエラーをラッピングします'''
+
+
+class RetryCountExceededError(WebAccessError):
     '''再試行回数を超過した際に送出されます'''
+
+
+class NoSuchDiaryError(TsLoveToolsException):
+    '''指定された日記が存在しなかった際に送出されます'''
