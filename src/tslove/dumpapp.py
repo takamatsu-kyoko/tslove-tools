7385b9852dff0988dba6f77aca64fe9e55cd68b1
Takamatsu.kyoko <38364117+takamatsu-kyoko@users.noreply.github.com>
2022-03-20 22:10:17 +0900

    DumpAppにスタイルシート取得関係のメソッドを追加

--- src/tslove/dumpapp.py
+++ src/tslove/dumpapp.py
@@ -11,6 +11,7 @@ from tslove.core.exception import WebAccessError
 
 class DumpApp():  # pylint: disable=R0903
     '''ダンプアプリケーションの基底クラス'''
+    STYLESHEET_URL_PATTERN = re.compile(r'url\((?P<path>.+)\)')
 
     def __init__(self) -> None:
         self._config: Any = None
@@ -114,3 +115,104 @@ class DumpApp():  # pylint: disable=R0903
             filename = os.path.basename(path)
 
         return filename
+
+    def _dump_stylesheet(self) -> None:
+        '''スタイルシートをダンプします
+
+        self._config の output_path 属性を利用します
+
+        :raises: WebAccessError スタイルシート本文の取得に失敗した場合
+        :raises: OSError ファイルの出力に失敗した場合
+        '''
+        assert hasattr(self._config, 'output_path')
+
+        stylesheet_file_name = os.path.join(self._config.output_path['stylesheet'], 'tslove.css')
+        if not os.path.exists(stylesheet_file_name):
+            try:
+                stylesheet = self._web.get_stylesheet()
+                image_paths = self.__collect_stylesheet_image_paths(stylesheet)
+                self.__fetch_stylesheet_images(image_paths)
+                self.__output_stylesheet(stylesheet, stylesheet_file_name)
+            except (WebAccessError, OSError) as err:
+                print('Can not get stylesheet. {}'.format(err))
+                raise err
+
+    # TODO この機能はStylesheetクラスを作成してそちらに移す
+    @staticmethod
+    def __collect_stylesheet_image_paths(stylesheet: str) -> set:
+        '''スタイルシートに含まれる画像ファイルのパスを収集します
+
+        :param stylesheet: スタイルシートの内容
+        :return: 画像ファイルのパスの集合
+        '''
+        paths = set()
+        for line in stylesheet.splitlines():
+            result = DumpApp.STYLESHEET_URL_PATTERN.search(line)
+            if result:
+                paths.add(result.group('path'))
+
+        return paths
+
+    # TODO 画像の取得は取得元,保存先の対のリストを入力として共通化する
+    def __fetch_stylesheet_images(self, image_paths: set, overwrite=False) -> None:
+        '''スタイルシートに含まれる画像を取得します
+
+        self._config の output_path 属性を利用します
+
+        画像の取得の失敗はメッセージの出力のみで処理を継続します。例外の送出はありません
+
+        :param image_paths: 取得する画像のパスの集合
+        :param overwrite: 画像を上書きする場合 True
+        '''
+        assert hasattr(self._config, 'output_path')
+
+        output_path = self._config.output_path['stylesheet']
+        exclude_path = ['./skin/default/img/marker.gif']
+
+        for path in image_paths:
+            if path in exclude_path:
+                continue
+
+            filename = os.path.join(output_path, self.__convert_stylesheet_image_path_to_filename(path))
+            if os.path.exists(filename) and overwrite is False:
+                continue
+
+            try:
+                image = self._web.get_image(path)
+                image.save(filename)
+            except (WebAccessError, OSError) as err:
+                print('Can not get stylesheets image {}. {}'.format(path, err))
+                continue
+
+    # TODO 画像の取得は取得元,保存先の対のリストを入力として共通化する
+    @staticmethod
+    def __convert_stylesheet_image_path_to_filename(path: str) -> str:
+        '''スタイルシートに含まれる画像のパスをファイル名に変換します
+
+        :param path: 変換前のpath
+        :return: 変換後のpath
+        '''
+        pattern = re.compile(r'image_filename=(?P<filename>[^&;?]+)')
+        result = pattern.search(path)
+        if result:
+            filename = result.group('filename')
+        else:
+            filename = os.path.basename(path)
+
+        return filename
+
+    def __output_stylesheet(self, stylesheet: str, file_name: str) -> None:
+        '''スタイルシートをファイルに出力します
+
+        :param stylesheet: スタイルシート本文
+        :param file_name: 出力先
+        :raises: OSError ファイルの書き込みに失敗した場合
+        '''
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for line in stylesheet.splitlines():
+                result = DumpApp.STYLESHEET_URL_PATTERN.search(line)
+                if result:
+                    old_path = result.group('path')
+                    new_path = './' + self.__convert_stylesheet_image_path_to_filename(old_path)
+                    line = line.replace(old_path, new_path)
+                file.write(line + '\n')
