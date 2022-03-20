fb55779c3800aee127f6b6c9d5dea9697012a274
Takamatsu.kyoko <38364117+takamatsu-kyoko@users.noreply.github.com>
2022-03-20 22:05:40 +0900

    DumpAppに画像取得関係のメソッドを追加

--- src/tslove/dumpapp.py
+++ src/tslove/dumpapp.py
@@ -2,6 +2,7 @@
 
 import getpass
 import os
+import re
 from typing import Any
 
 from tslove.core.web import TsLoveWeb
@@ -59,3 +60,57 @@ class DumpApp():  # pylint: disable=R0903
         except OSError as err:
             print('Can not create directory. {}'.format(err))
             raise err
+
+    # TODO 画像の取得は取得元,保存先の対のリストを入力として共通化する
+    def _fetch_images(self, image_paths: set, overwrite=False):
+        '''画像を取得します
+
+        self._config の output_path 属性を利用します
+
+        画像の取得の失敗はメッセージの出力のみで処理を継続します。例外の送出はありません
+
+        :param image_paths: 取得する画像のパスの集合
+        :param overwrite: 画像を上書きする場合 True
+        '''
+        assert hasattr(self._config, 'output_path')
+
+        output_path = self._config.output_path['image']
+
+        for path in image_paths:
+            if '://' in path:
+                continue
+
+            filename = os.path.join(output_path, self._convert_image_path_to_filename(path))
+            if os.path.exists(filename) and overwrite is False:
+                continue
+
+            try:
+                if 'img.php' in path:
+                    params = {'m': 'pc',
+                              'filename': self._convert_image_path_to_filename(path),
+                              }
+                    image = self._web.get_image('img.php', params)
+                else:
+                    image = self._web.get_image(path)
+                image.save(filename)
+            except (WebAccessError, OSError) as err:
+                print('Can not get image {}. {}'.format(path, err))
+                continue
+
+    # TODO 画像の取得は取得元,保存先の対のリストを入力として共通化する
+    # DiaryDumpApp.fix_linkが使う
+    @staticmethod
+    def _convert_image_path_to_filename(path: str) -> str:
+        '''画像のパスをファイル名に変換します
+
+        :param path: 変換前のpath
+        :return: 変換後のpath
+        '''
+        pattern = re.compile(r'filename=(?P<filename>[^&;?]+)')
+        result = pattern.search(path)
+        if result:
+            filename = result.group('filename')
+        else:
+            filename = os.path.basename(path)
+
+        return filename
